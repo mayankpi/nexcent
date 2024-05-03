6aa734f9aa6aa81ddd6e5d90003e1b6978f89717
mayankpi <mayankm522@gmail.com>
2024-05-04 02:29:30 +0530

    full site

---
+++ src/components/Home.jsx
@@ -0,0 +1,80 @@
+import { Carousel } from "flowbite-react";
+import code from "../assets/code.jpg";
+const Home = () => {
+    return (
+        <div className="bg-neutralSilver" id="home">
+            <div className="px-4 lg:px-14 max-w-screen-2xl mx-auto min-h-screen h-screen">
+                <Carousel className="w-full mx-auto">
+                    <div className="my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse items-center justify-normal gap-12 ">
+                        <div>
+                            <img
+                                className="w-3/4 h-3/4"
+                                src={code}
+                                alt="code"
+                            />
+                        </div>
+                        <div className="md:w-1/2">
+                            <h1 className="text-5xl font-semibold mb-4 text-neutralDGray md:w-3/4 leading-snug">
+                                Lets Learn and earn Money{" "}
+                                <span className="text-brandPrimary leading-snug">
+                                    in 4 months
+                                </span>
+                            </h1>
+                            <p className="text-neutralGray text-base mb-8">
+                                Where to grow your business as a photographer:
+                                site or social media?
+                            </p>
+                            <button className="btn-primary">Register</button>
+                        </div>
+                    </div>
+                    <div className="my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse items-center justify-normal gap-12 ">
+                        <div>
+                            <img
+                                className="w-3/4 h-3/4"
+                                src={code}
+                                alt="code"
+                            />
+                        </div>
+                        <div className="md:w-1/2">
+                            <h1 className="text-5xl font-semibold mb-4 text-neutralDGray md:w-3/4 leading-snug">
+                                Lessons and insights{" "}
+                                <span className="text-brandPrimary leading-snug">
+                                    from 8 years
+                                </span>
+                            </h1>
+                            <p className="text-neutralGray text-base mb-8">
+                                Where to grow your business as a photographer:
+                                site or social media?
+                            </p>
+                            <button className="btn-primary">Register</button>
+                        </div>
+                    </div>
+                    <div className="my-28 md:my-8 py-12 flex flex-col md:flex-row-reverse items-center justify-normal gap-12 ">
+                        <div>
+                            <img
+                                className="w-3/4 h-3/4"
+                                src={code}
+                                alt="code"
+                            />
+                        </div>
+                        <div className="md:w-1/2">
+                            <h1 className="text-5xl font-semibold mb-4 text-neutralDGray md:w-3/4 leading-snug">
+                                Lessons and insights{" "}
+                                <span className="text-brandPrimary leading-snug">
+                                    from 8 years
+                                </span>
+                            </h1>
+                            <p className="text-neutralGray text-base mb-8">
+                                Where to grow your business as a photographer:
+                                site or social media?
+                            </p>
+                            <button className="btn-primary">Register</button>
+                        </div>
+                    </div>
+                </Carousel>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
